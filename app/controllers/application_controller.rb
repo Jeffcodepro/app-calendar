bbdad9f6ad0b01cae6b15e94a8f3ab17996a8e83
Jefferson Santos <jeffersonoliveira1212@gmail.com>
2026-01-28 14:52:12 -0300

    teste de envio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
+# teste
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
       user_params.permit(:first_name, :last_name, :role, :email, :password, :password_confirmation)
